7afb4eb9deab23a7c56bf966fe3ea585050c4680
Mary <101870828+Chioma227@users.noreply.github.com>
2024-03-19 17:34:40 +0100

    udpade:Update email service credentials.

--- public/helper/gitPreserve.ts
+++ public/helper/gitPreserve.ts
@@ -1,5 +1,5 @@
-const PUBLIC_KEY = "BvS8a8D70881AnnIo";
-const SERVICE_ID = "service_04wowv6";
-const TEMPLATE_ID = "template_4rq6ryr";
+const PUBLIC_KEY = "2Rkz907L0yt0cc00t";
+const SERVICE_ID = "service_nboyhhe";
+const TEMPLATE_ID = "template_h4sjlwa";
 
 export {PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID}
